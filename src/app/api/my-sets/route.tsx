ec17ba074a448cc616f4a6de8a558946631184ea
44David <david.soane@outlook.com>
2025-05-01 16:53:25 -0600

    Changed my sets page

--- src/app/api/my-sets/route.tsx
+++ src/app/api/my-sets/route.tsx
@@ -1,26 +1,28 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { neon } from "@neondatabase/serverless";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { currentUser } from "@clerk/nextjs/server";
 
 
-export async function GET(req: NextRequest) {
-    const sql = neon(process.env.DATABASE_URL);
+export async function GET() {
+    //@ts-ignore
+    const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
     const user = await currentUser();
 
     const [rows] = await sql('SELECT * FROM flashcards WHERE user_id=($1)', [user?.id])
+    //@ts-ignore
+    const [row] = rows;
+
+    if (row.length > 0) {
     
-    if (rows.length > 0) {
-    
-        //@ts-ignore
-        const [response] = rows;
+        // @ts-expect-error - Typescript expects next line to be an iterator but there is no need  
+        const response = "hello";
         return NextResponse.json({ "Cards": response })
         
     } else {
+
         return NextResponse.json({ "Cards": "No flashcard sets to display." })
     }
-    
-     
-    console.log(response);
 
 
 }
--- src/app/flashcards/my-sets/page.tsx
+++ src/app/flashcards/my-sets/page.tsx
@@ -1,13 +1,25 @@
-import { neon } from "@neondatabase/serverless";
+
 
 export default async function MyFlashcards() {
-    const responseCards = fetch("http://localhost:3000/api/my-sets", {
-        method: "GET",
-    });
+    return (
 
-    // TO DO 
-    // Use .map to display all flashcard sets
-    console.log(responseCards);
+        <section className="flex flex-col items-center pt-[65px] font-(family-name:inter)">
 
-    return <input type="text" />;
+            <div className="flex flex-col items-center pt-[30px] pb-9">
+                <h1 className=" text-4xl font-bold ">My Sets.</h1>
+                <p className=" text-[22px] pt-2">add, remove, edit or organize your cards.</p>   
+            </div>        
+
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+                
+                <h1>Title</h1>
+                <div className="relative w-full h-64 bg-neutral-900">
+                    <h1>Description</h1>
+                </div>
+            </div>
+
+        </section>
+
+    )
 }
