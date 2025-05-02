9db44c7b64e4f74475af88c42bcbdf7de934d26d
Pollick0 <salman.shakhawat@outlook.com>
2025-05-01 22:28:44 -0600

    added custom css, changed card structure, updated design for create page

--- src/app/flashcards/create/page.tsx
+++ src/app/flashcards/create/page.tsx
@@ -27,22 +27,27 @@ export default function Create() {
         };
     }, []);
 
-    const [selectedIndex, setSelectedIndex] = useState(null);
-    const sets = [
-        'Set 1',
-        'Set 2',
-        'Set 3',
-        'Set 4',
-        'Set 5',
-        'Set 6',
-        'Set 7',
-        'Set 8',
-        'Set 9',
-    ];
+    const [selectedKey, setSelectedKey] = useState("");
+    const [selectedDict, setSelectedDict] = useState("");
 
-    const handleSelect = (index) => {
-        setSelectedIndex(index);
-      };
+    const userSets = {
+        "Chinese Chapter 1": {
+            "Description": "Chinese flashcards for lesson 1",
+            "Cards": [
+                {"category": "vocab", "front": "Hello", "back": "Ni hao"}, 
+                {"category": "vocab", "front": "Why?", "back": "Wei shenme"},
+                {"category": "grammar", "front": "what?", "back": "shenme?"}
+            ] 
+        },
+        "German Chapter 34": {
+            "Description": "German flashcards for lesson 6",
+            "Cards": [
+                {"category": "vocab", "front": "Hello", "back": "Hallo"}, 
+                {"category": "vocab", "front": "Why?", "back": "warum?"},
+                {"category": "grammar", "front": "what?", "back": "was?"}
+            ]
+        }
+    }
 
     return (
 
@@ -56,22 +61,24 @@ export default function Create() {
 
 
             {/* Set Selection / Description */}
-            <div className="flex justify-between h-[150px] w-[1150px] bg-[#D9D9D9]/3 py-3 px-4 rounded-[5px]">
+            <div className="flex justify-between h-[150px] w-[1150px] bg-[#D9D9D9]/3 py-3 px-4 rounded-[10px]">
                 <div className="flex flex-col justify-between h-full">
                     <div>
-                        <h2 className="font-bold text-2xl">{sets[selectedIndex]}</h2>
-                        <p>{`My Set Description`}</p>
+                        <h2 className="font-bold text-2xl">{selectedKey == "" ? 'No Set Selected' : selectedKey}</h2>
+                        <p>{selectedKey == "" ? '' : selectedDict.Description}</p>
                     </div>
-
-                    <div>
-                        <p>{`0`} Cards</p>
-                    </div>
+                    {selectedDict && selectedDict.Cards && (
+                        <div>
+                            <p>{selectedDict.Cards.length} Cards</p>
+                        </div>
+                    )}
                 </div>
 
+                {/* Select Set Drop Down */}
                 <div className="flex gap-[11px]">
                     <div className="select-set-dd">
                         <button 
-                            className="flex justify-between items-center w-[200px] h-[40px] py-1 px-3 bg-[#D9D9D9]/3 rounded-[5px] border-1 border-[#828282]"
+                            className="flex justify-between items-center w-[250px] h-[40px] py-1 px-3 bg-[#D9D9D9]/3 rounded-[5px] border-1 border-[#828282] hover-animation"
                             onClick={toggleDropDown}
                             >
 
@@ -85,34 +92,41 @@ export default function Create() {
 
                         </button>
 
+                        {/* Drop Drown Content */}
                         {isOpen && (
                             <div className="absolute mt-1 flex flex-col gap-2 overflow-y-auto w-[200px] max-h-[300px] py-2 bg-[#202020] rounded-[5px] border-1 border-[#828282] hidden-scrollbar">
-                                {sets.map((set, index) => (
+                                {Object.entries(userSets).map(([key, value], index) => (
                                     <div 
                                         key={index} 
-                                        className="bg-[#202020] hover:bg-[#323232] flex rounded-[5px] py-[3px] pl-2 gap-x-4 mx-2"
-                                        onClick={() => handleSelect(index)}
-                                        >
-                                        {selectedIndex == index && (
+                                        className="bg-[#202020] flex rounded-[5px] py-[3px] pl-1 gap-x-2 mx-2 hover-animation overflow-x-scroll whitespace-nowrap hide-scrollbar"
+                                        onClick={() => {
+                                            setSelectedKey(key)
+                                            setSelectedDict(value)
+                                        }}
+                                    >
+                                        {selectedKey == key && (
                                             <Image src="/icons/checkmark.svg" alt="checkmark icon" width={24} height={24}/>
                                         )}
-                                        {set}
+                                        <div className="overflow-x-auto max-w-[200px] hide-scrollbar">{key}</div>
                                     </div>
                                 ))}
                             </div>
                         )}        
-
                     </div>
-
+                    
+                    {/* New Set Button */}
                     <div className="new-set-btn">
-                        <button className="flex justify-between items-center w-[125px] h-[40px] py-1 px-3 bg-[#D9D9D9]/3 rounded-[5px] border-1 border-[#828282] ">
+                        <button 
+                            className="flex justify-between items-center w-[125px] h-[40px] py-1 px-3 bg-[#D9D9D9]/3 rounded-[5px] border-1 border-[#828282] hover:bg-[#474747] transition-colors duration-200"
+                            onClick={}
+                        >
                             <Image
                                 src="/icons/add.svg"
                                 alt="add icon"
                                 width={20}
                                 height={20}
                             />
-                            {`New Set`}
+                            New Set
                         </button>
                     </div>
                 </div>
