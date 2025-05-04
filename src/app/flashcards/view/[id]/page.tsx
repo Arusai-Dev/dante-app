21d910e96bbd5574298589ea1afdd2d925b55888
44David <david.soane@outlook.com>
2025-05-04 17:14:28 -0600

    Successfully parse json and render individual cards on /view page

--- src/app/flashcards/view/[id]/page.tsx
+++ src/app/flashcards/view/[id]/page.tsx
@@ -1,18 +1,37 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { getSet } from "@/lib/dbFunctions"
 
-
 export default async function ViewSet({ params }) {  
     const { id } = await params;
     
     //@ts-ignore
     const [set] = await getSet(id)
 
+    const jsonCards = JSON.parse(set.cards);
+
     return (
 
-        <div className="flex items-center justify-center">
-            <p className="text-center">{set.title}</p>
-        </div>
+        <section className="flex flex-col items-center pt-[65px] font-(family-name:inter)">
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-3 gap-8 h-full 2xl:grid-cols-3">
+
+                { jsonCards.map((card, index:number) => (
+                        <div key={index} className="w-[512px]">
+
+                            <h1 className="relative font-bold text-2xl">
+                                {card.front}
+                            </h1>
+
+                            <div className="flex relative content-center items-center text-center h-64 w-full bg-neutral-900 rounded-2xl">
+                                {card.back}
+                            </div>
+
+                        </div>
+                ))}
+
+            </div>
+
+        </section>
 
     ) 
 }
